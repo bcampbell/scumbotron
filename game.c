5feef576c4caf149f5344a2e8758fc79b86e2ced
Ben Campbell <ben@scumways.com>
2022-10-17 10:21:00 +1300

    implement dudes

--- game.c
+++ game.c
@@ -37,7 +37,195 @@ void testum() {
 
 
 
-void init_display()
+typedef struct dude {
+    int8_t kind;
+    int8_t dat;
+    int16_t x;
+    int16_t y;
+} dude;
+
+// Dude flags
+#define DF_LIVE 0x01
+
+
+// Dude Kinds
+#define DK_NONE 0
+#define DK_PLAYER 1
+#define DK_SHOT 2
+#define DK_BLOCK 3
+#define DK_GRUNT 4
+
+#define MAX_DUDES 80
+
+dude dudes[MAX_DUDES];
+
+
+void player_tick(dude* d);
+void shot_tick(dude* d);
+void grunt_tick(dude* d);
+
+
+void dudes_init()
+{
+    for (uint8_t i = 0; i < MAX_DUDES; ++i) {
+        dude* d = &dudes[i];
+        d->kind = DK_NONE;
+    }
+}
+
+void dudes_tick()
+{
+    for (uint8_t i = 0; i < MAX_DUDES; ++i) {
+        dude* d = &dudes[i];
+        switch(d->kind) {
+            case DK_NONE:
+                break;
+            case DK_PLAYER:
+                player_tick(d);
+                break;
+            case DK_SHOT:
+                shot_tick(d);
+                break;
+            case DK_BLOCK:
+                break;
+            case DK_GRUNT:
+                grunt_tick(d);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+
+void sproff() {
+    const int16_t x = -16;
+    const int16_t y = -16;
+    VERA.data0 = ((0x10000+256)>>5) & 0xFF;
+    VERA.data0 = (1 << 7) | ((0x10000+256)>>13);
+    VERA.data0 = x & 0xff;  // x lo
+    VERA.data0 = (x>>8) & 0x03;  // x hi
+    VERA.data0 = y & 0xff;  // y lo
+    VERA.data0 = (y>>8) & 0x03;  // y hi
+    VERA.data0 = 3<<2; // collmask(4),z(2),vflip,hflip
+    VERA.data0 = (1 << 6) | (1 << 4);  // 16x16, 0 palette offset.
+}
+
+void sprout(int16_t x, int16_t y, uint8_t idx) {
+    VERA.data0 = ((0x10000+256*idx)>>5) & 0xFF;
+    VERA.data0 = (1 << 7) | ((0x10000+256*idx)>>13);
+    VERA.data0 = x & 0xff;  // x lo
+    VERA.data0 = (x>>8) & 0x03;  // x hi
+    VERA.data0 = y & 0xff;  // y lo
+    VERA.data0 = (y>>8) & 0x03;  // y hi
+    VERA.data0 = 3<<2; // collmask(4),z(2),vflip,hflip
+    VERA.data0 = (1 << 6) | (1 << 4);  // 16x16, 0 palette offset.
+}
+
+void dudes_render()
+{
+    VERA.control = 0x00;
+    // sprite attrs start at 0x1FC00
+    VERA.address = 0xFC00;
+    VERA.address_hi = VERA_INC_1 | 0x01; // hi bit = 1
+
+    for (uint8_t i = 0; i < MAX_DUDES; ++i) {
+        dude* d = &dudes[i];
+        switch(d->kind) {
+            case DK_NONE:
+                sproff();
+                break;
+            case DK_PLAYER:
+                sprout(d->x, d->y, 0);
+                break;
+            case DK_SHOT:
+                sprout(d->x, d->y, 1);
+                break;
+            case DK_BLOCK:
+                sprout(d->x, d->y, 2);
+                break;
+            case DK_GRUNT:
+                sprout(d->x, d->y, 3 + (tick >> 2) & 0x03);
+                break;
+            default:
+                sproff();
+                break;
+        }
+    }
+}
+
+void dude_init(dude* d, uint8_t kind, int x, int y) {
+    d->kind = kind;
+    d->x = x;
+    d->y = y;
+    d->dat = 0;
+}
+
+dude* dude_alloc() {
+    for(uint8_t i=0; i<MAX_DUDES; ++i) {
+        if(dudes[i].kind == DK_NONE) {
+            return &dudes[i];
+        }
+    }
+    return 0;
+}
+
+
+// player
+
+void player_tick(dude* d) {
+
+    if ((inp_joystate & JOY_UP_MASK) ==0) {
+        d->y -= 2;
+    } else if ((inp_joystate & JOY_DOWN_MASK) ==0) {
+        d->y += 2;
+    }
+    if ((inp_joystate & JOY_LEFT_MASK) ==0) {
+        d->x -= 2;
+    } else if ((inp_joystate & JOY_RIGHT_MASK) ==0) {
+        d->x += 2;
+    }
+
+    if ((inp_joystate & JOY_BTN_1_MASK) == 0) {
+        dude* shot = dude_alloc();
+        if (shot) {
+            dude_init(shot, DK_SHOT, d->x, d->y);
+        }
+    }
+}
+
+// shot
+void shot_tick(dude* d)
+{
+    ++d->dat;
+    if (d->dat>30) {
+        d->kind = DK_NONE;
+    }
+}
+
+// grunt
+void grunt_tick(dude* d)
+{
+    d->dat++;
+    if (d->dat>13) {
+        d->dat = 0;
+        dude* plr = &dudes[0];
+        if (plr->x < d->x) {
+            --d->x;
+        } else if (plr->x > d->x) {
+            ++d->x;
+        }
+        if (plr->y < d->y) {
+            --d->y;
+        } else if (plr->y > d->y) {
+            ++d->y;
+        }
+    }
+
+}
+
+
+void display_init()
 {
     // screen mode 40x30
     VERA.control = 0x00;    //DCSEL=0
@@ -50,7 +238,7 @@ void init_display()
     VERA.display.hscale = 64;   // 2x scale
     VERA.display.vscale = 64;   // 2x scale
     VERA.control = 0x02;    //DCSEL=1
-    VERA.display.hstart = 0;
+    VERA.display.hstart = 2;    // 2 to allow some border for rastertiming
     VERA.display.hstop = 640>>2;
     VERA.display.vstart = 0;
     VERA.display.vstop = 480>>1;
@@ -102,68 +290,28 @@ void init_display()
     }
 }
 
-void showsprite(int16_t x, int16_t y, uint8_t i, uint8_t off) {
-    VERA.control = 0x00;
-    // sprite attrs start at 0x1FC00
-    VERA.address = 0xFC00 + (i*8);
-    VERA.address_hi = VERA_INC_1 | 0x01; // hi bit = 1
-
-    VERA.data0 = ((0x10000+256*i)>>5) & 0xFF;
-    VERA.data0 = (1 << 7) | ((0x10000+256*i)>>13);
-    VERA.data0 = x & 0xff;  // x lo
-    VERA.data0 = (x>>8) & 0x03;  // x hi
-    VERA.data0 = y & 0xff;  // y lo
-    VERA.data0 = (y>>8) & 0x03;  // y hi
-    VERA.data0 = 3<<2; // collmask(4),z(2),vflip,hflip
-    VERA.data0 = (1 << 6) | (1 << 4);  // 64x64, 0 palette offset.
-}
-
-
-int16_t obx[16] = {0};
-int16_t oby[16] = {0};
-
-
-void tick_player(uint8_t ob) {
-
-    if ((inp_joystate & JOY_UP_MASK) ==0) {
-        oby[ob] -= 2;
-    } else if ((inp_joystate & JOY_DOWN_MASK) ==0) {
-        oby[ob] += 2;
-    }
-    if ((inp_joystate & JOY_LEFT_MASK) ==0) {
-        obx[ob] -= 2;
-    } else if ((inp_joystate & JOY_RIGHT_MASK) ==0) {
-        obx[ob] += 2;
-    }
-    //if ((inp_joystate & JOY_BTN_1_MASK) ==0) {
-    //}
-}
-
-
 int main(void) {
-    init_display();
-    // a little border on the left for rastertiming
-//    VERA.display.hstart=2;
+    display_init();
 
     irq_init();
-    testum();
 
-    // ENABLE SPRITES DAMMIT!!!
-    VERA.display.video |= 0x40;
+    dudes_init();
+    dude_init(&dudes[0], DK_PLAYER, (320/2) + 8, (240/2)+8);
 
-    int16_t x = 0;
-    int16_t y = 0;
-    uint8_t i=0;
-    uint8_t off=0;
+    uint8_t i=1;
+    for( uint8_t y=0; y<8; ++y) {
+        for( uint8_t x=0; x<8; ++x) {
+            dude_init(&dudes[i++], DK_GRUNT, 4+x*38,4+y*38);
+        }
+    }
     while (1) {
         VERA.display.border = 2;
-        for (i=0; i<16; ++i) {
-            showsprite(obx[i], oby[i], i, off);
-        }
-        VERA.display.border = 1;
+        dudes_render();
+        VERA.display.border = 3;
         testum();
         inp_tick();
-        tick_player(0);
+        VERA.display.border = 15;
+        dudes_tick();
         VERA.display.border = 0;
         waitvbl();
     }
