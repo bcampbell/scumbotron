2d8aa29e411f6c2ef119fb52fc5546a59c2d6537
Ben Campbell <ben@scumways.com>
2022-10-16 14:30:10 +1300

    move player, set screen mode, load palette

--- Makefile
+++ Makefile
@@ -4,6 +4,14 @@ game.prg: game.c gfx.c irq.s glue.s
 
 gfx.c: sprites16.png
 	echo "#include <stdint.h>" >gfx.c
+	echo "uint8_t palette[16*2] = {" >>gfx.c
+	tools/png2sprites -p sprites16.png >>gfx.c
+	echo "};" >>gfx.c
 	echo "uint8_t sprites[16*16*16] = {" >>gfx.c
 	tools/png2sprites -n 16 -w 16 -h 16 sprites16.png >>gfx.c
 	echo "};" >>gfx.c
+
+run: game.prg
+	x16emu -prg game.prg -run
+
+
--- game.c
+++ game.c
@@ -19,6 +19,7 @@ extern void inp_tick();
 extern volatile uint16_t inp_joystate;
 
 extern uint8_t sprites[16*16*16];
+extern uint8_t palette[16*2];   // just 16 colours
 
 void testum() {
     VERA.control = 0x00;
@@ -28,23 +29,76 @@ void testum() {
     uint16_t b = inp_joystate;
     for (uint8_t i=0; i<16; ++i) {
         // char, then colour
-        VERA.data0 = (b & 0x8000) ? 'X' : '.';
+        VERA.data0 = (b & 0x8000) ? '.' : 'X';
         VERA.data0 = COLOR_BLACK<<4 | COLOR_GREEN;
         b = b<<1;
     }
 }
 
 
-void loadsprites() {
-    VERA.control = 0x00;
-    // Writing to 0x10000 onward.
-    VERA.address = 0x0000;
-    VERA.address_hi = 0x11; // increment = 1
 
-    // 16 16*16 images
-    const uint8_t* p = sprites;
-    for (int i=0; i<16*16*16; ++i) {
-        VERA.data0 = *p++;
+void init_display()
+{
+    // screen mode 40x30
+    VERA.control = 0x00;    //DCSEL=0
+    uint8_t vid = VERA.display.video;
+    vid |= 1<<6;    // enable sprites
+    vid |= 1<<5; // layer 1 on
+    vid &= ~(1<<4);    // layer 1 off
+    VERA.display.video = vid;
+
+    VERA.display.hscale = 64;   // 2x scale
+    VERA.display.vscale = 64;   // 2x scale
+    VERA.control = 0x02;    //DCSEL=1
+    VERA.display.hstart = 0;
+    VERA.display.hstop = 640>>2;
+    VERA.display.vstart = 0;
+    VERA.display.vstop = 480>>1;
+
+    // layer setup
+    VERA.layer1.config = 0x10;    // 64x32 tiles, !T256C, !bitmapmode, 1bpp
+//    VERA.layer0.mapbase = 
+//    VERA.layer0.tilebase = 
+    VERA.layer1.hscroll = 0; // 16bit
+    VERA.layer1.vscroll = 0; // 16bit
+
+    // load the palette to vram (fixed at 0x1FA00 onward)
+    {
+        VERA.control = 0x00;
+        VERA.address = 0xFA00;
+        VERA.address_hi = VERA_INC_1 | 0x01; // hi bit = 1
+        const uint8_t* src = palette;
+        // just 16 colours
+        for (int i=0; i<16*2; ++i) {
+            VERA.data0 = *src++;
+        }
+    }
+
+    // load the sprite images to vram (0x10000 onward)
+    {
+        VERA.control = 0x00;
+        VERA.address = 0x0000;
+        VERA.address_hi = VERA_INC_1 | 0x01; // hi bit = 1
+
+        // 16 16*16 images
+        const uint8_t* src = sprites;
+        for (int i=0; i<16*16*16; ++i) {
+            VERA.data0 = *src++;
+        }
+    }
+
+    // clear the screen (0x1B0000)
+    {
+        VERA.control = 0x00;
+        VERA.address = 0xB000;
+        VERA.address_hi = VERA_INC_1 | 0x01; // hi bit = 1
+
+        // 16 16*16 images
+        const uint8_t* src = sprites;
+        for (int i=0; i<64*32; ++i) {
+            VERA.data0 = ' '; // tile
+            VERA.data0 = 0; // colour
+        }
     }
 }
 
@@ -65,55 +119,34 @@ void showsprite(int16_t x, int16_t y, uint8_t i, uint8_t off) {
 }
 
 
-void bg(uint8_t r, uint8_t g, uint8_t b) {
-    VERA.control = 0x00;
-    // sprite attrs start at 0x1FC00
-    VERA.address = 0xFA00;
-    VERA.address_hi = VERA_INC_1 | 0x01; // hi bit = 1
-
-    VERA.data0 = (g & 0xf0) | (b>>4);
-    VERA.data0 = (r>>4);
-}
-
-
 int16_t obx[16] = {0};
 int16_t oby[16] = {0};
 
 
-
-#define INP_B 0x80
-#define INP_Y 0x40
-#define INP_SELECT 0x20
-#define INP_START 0x10
-#define INP_UP 0x08
-#define INP_DOWN 0x04
-#define INP_LEFT 0x02
-#define INP_RIGHT 0x01
-
 void tick_player(uint8_t ob) {
 
-    if ((inp_joystate & INP_UP) ==0) {
-        oby[ob] -= 4;
-    } else if ((inp_joystate & INP_DOWN) ==0) {
-        oby[ob] += 4;
+    if ((inp_joystate & JOY_UP_MASK) ==0) {
+        oby[ob] -= 2;
+    } else if ((inp_joystate & JOY_DOWN_MASK) ==0) {
+        oby[ob] += 2;
     }
-    if ((inp_joystate & INP_LEFT) ==0) {
-        obx[ob] -= 4;
-    } else if ((inp_joystate & INP_RIGHT) ==0) {
-        obx[ob] += 4;
+    if ((inp_joystate & JOY_LEFT_MASK) ==0) {
+        obx[ob] -= 2;
+    } else if ((inp_joystate & JOY_RIGHT_MASK) ==0) {
+        obx[ob] += 2;
     }
+    //if ((inp_joystate & JOY_BTN_1_MASK) ==0) {
+    //}
 }
 
 
 int main(void) {
+    init_display();
     // a little border on the left for rastertiming
-    VERA.control = 0x02;    //DCSEL=1
-    VERA.display.hstart=2;
-    VERA.control = 0x00;
+//    VERA.display.hstart=2;
 
     irq_init();
     testum();
-    loadsprites();
 
     // ENABLE SPRITES DAMMIT!!!
     VERA.display.video |= 0x40;
