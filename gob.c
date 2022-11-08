8f7664276a2ea182b5694aebec57974712845d6c
Ben Campbell <ben@scumways.com>
2022-11-08 21:22:46 +1300

    amoeba movement improvement

--- gob.c
+++ gob.c
@@ -497,19 +497,33 @@ void amoeba_init(uint8_t d)
 
 void amoeba_tick(uint8_t d)
 {
-    // update every 16 frames
+    // jitter acceleration
+    gobvx[d] += (rnd() - 128)>>2;
+    gobvy[d] += (rnd() - 128)>>2;
+
+
+    // apply drag
+    int16_t vx = gobvx[d];
+    gobvx[d] = (vx >> 1) + (vx >> 2);
+    int16_t vy = gobvy[d];
+    gobvy[d] = (vy >> 1) + (vy >> 2);
+    
+    const int16_t AMOEBA_MAX_SPD = (4<<FX)/3;
+    const int16_t AMOEBA_ACCEL = 2 * FX_ONE / 2;
+
+    gobx[d] += gobvx[d];
+    goby[d] += gobvy[d];
+
+    // update homing every 16 frames
     if (((tick+d) & 0x0f) != 0x00) {
        return;
     }
-    const int16_t AMOEBA_MAX_SPD = 1<<FX;
-    const int16_t AMOEBA_ACCEL = 1<<FX;
     int16_t px = gobx[0];
     if (px < gobx[d] && gobvx[d] > -AMOEBA_MAX_SPD) {
         gobvx[d] -= AMOEBA_ACCEL;
     } else if (px > gobx[d] && gobvx[d] < AMOEBA_MAX_SPD) {
         gobvx[d] += AMOEBA_ACCEL;
     }
-    gobx[d] += gobvx[d];
 
     int16_t py = goby[0];
     if (py < goby[d] && gobvy[d] > -AMOEBA_MAX_SPD) {
@@ -517,7 +531,8 @@ void amoeba_tick(uint8_t d)
     } else if (py > goby[d] && gobvy[d] < AMOEBA_MAX_SPD) {
         gobvy[d] += AMOEBA_ACCEL;
     }
-    goby[d] += gobvy[d];
+
+
 
 }
 
@@ -543,18 +558,20 @@ void amoeba_shot(uint8_t d)
     }
 
     if (gobkind[d] == GK_AMOEBA_MED) {
+        const int16_t v = FX<<5;
         gobkind[d] = GK_NONE;
-        amoeba_spawn(GK_AMOEBA_SMALL, gobx[d], goby[d], -(FX/2), FX/2);
-        amoeba_spawn(GK_AMOEBA_SMALL, gobx[d], goby[d], FX/2, FX/2);
-        amoeba_spawn(GK_AMOEBA_SMALL, gobx[d], goby[d], 0, -(FX/2));
+        amoeba_spawn(GK_AMOEBA_SMALL, gobx[d], goby[d], -v, v);
+        amoeba_spawn(GK_AMOEBA_SMALL, gobx[d], goby[d], v, v);
+        amoeba_spawn(GK_AMOEBA_SMALL, gobx[d], goby[d], 0, -v);
         return;
     }
 
     if (gobkind[d] == GK_AMOEBA_BIG) {
+        const int16_t v = FX<<5;
         gobkind[d] = GK_NONE;
-        amoeba_spawn(GK_AMOEBA_MED, gobx[d], goby[d], -(FX/2), FX/2);
-        amoeba_spawn(GK_AMOEBA_MED, gobx[d], goby[d], FX/2, FX/2);
-        amoeba_spawn(GK_AMOEBA_MED, gobx[d], goby[d], 0, -(FX/2));
+        amoeba_spawn(GK_AMOEBA_MED, gobx[d], goby[d], -v, v);
+        amoeba_spawn(GK_AMOEBA_MED, gobx[d], goby[d], v, v);
+        amoeba_spawn(GK_AMOEBA_MED, gobx[d], goby[d], 0, -v);
         return;
     }
 }
