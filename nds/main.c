0974bd1cc008524a6c298bbc4ed55cbaf14c9f9b
Ben Campbell <ben@scumways.com>
2023-01-06 18:03:35 +1300

    nds: hud

--- nds/main.c
+++ nds/main.c
@@ -2,7 +2,9 @@
 #include "../sys.h"
 #include "../gob.h" // for ZAPPER_*
 
+// sprintf support...
 #include <stdio.h>
+#include <inttypes.h>
 
 // Combining the screens into a single play area.
 // These are the screen extents (in pixels).
@@ -108,7 +110,22 @@ void sys_text(uint8_t cx, uint8_t cy, const char* txt, uint8_t colour)
 
 void sys_hud(uint8_t level, uint8_t lives, uint32_t score)
 {
-//	iprintf("\x1b[0;l0Hlv %d  score: %d  lives: %d", (int)level, (int)score, (int)lives);
+    char buf[40];
+    uint8_t i;
+    sprintf(buf, "LV %" PRIu8 "", level);
+    sys_text(0, 0, buf, 1);
+
+    sprintf(buf,"%08" PRIu32 "", score);
+    sys_text(8, 0, buf, 1);
+
+    for (i = 0; i < 7 && i < lives; ++i) {
+        buf[i] = '*';
+    }
+    if (lives > 7) {
+        buf[i++] = '+';
+    }
+    buf[i] = '\0';
+    sys_text(18, 0, buf, 3);
 }
 
 // sprite image defs
