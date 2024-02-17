bc7d67e78211e0e277af29b3a5911057a9462e40
Ben Campbell <ben@scumways.com>
2024-02-18 10:52:12 +1300

    null build - add missing include

---
+++ null/plat_details.h
@@ -0,0 +1,11 @@
+#ifndef PLAT_DETAILS_H
+#define PLAT_DETAILS_H
+
+#define SCREEN_W 320
+#define SCREEN_H 240
+
+// #define PLAT_HAS_MOUSE
+// #define PLAT_HAS_TEXTENTRY
+// #define PLAT_HAS_SCORESAVE
+
+#endif // PLAT_DETAILS_H
